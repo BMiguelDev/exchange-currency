d0debd7545a34472af365cb434b47ba5b5e3542f
Bruno Miguel <113399299+BMiguelDev@users.noreply.github.com>
2024-02-02 13:03:02 +0000

    Add asynchronous API call and state variable
    - Add asynchronous API call with a "getTicker" query, store it in a state variable, and show data on the screen
    - Add model.ts file containing the type definition for the API retrieved data

--- src/components/CurrencyConverter/CurrencyConverter.tsx
+++ src/components/CurrencyConverter/CurrencyConverter.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SDK from "@uphold/uphold-sdk-javascript";
+
+import { APICurrencyRatePair } from "../../models/model";
 
 // Initialize Uphold's SDK
 const sdk = new SDK({
@@ -11,10 +13,21 @@ const sdk = new SDK({
 const CurrencyConverter = () => {
     // Set state and functions here that will be shared/needed by the children components
 
+    const [data, setData] = useState<APICurrencyRatePair[]>([]);
+
     useEffect(() => {
-        sdk.getTicker().then((data: any) => {
-            console.log(data);
-        });
+        const getCurrencyList = async () => {
+            try {
+                const data: APICurrencyRatePair[] = await sdk.getTicker();
+                setData(data);
+            } catch (err) {
+                console.log("Error retrieving currencies from Uphold API");
+                console.error(err);
+                alert("Server unreachable, try again later"); // If initial API call is unsuccessful, it most likely means the user has no internet connection or the server is unreachable
+            }
+        };
+
+        getCurrencyList();
     }, []);
 
     return (
@@ -25,7 +38,18 @@ const CurrencyConverter = () => {
                 <div>Input</div>
                 <div>Select</div>
             </div>
-            <section>Exchanged Currencies List</section>
+            <section>
+                Exchanged Currencies List
+                {
+                    data.map(dataItem=> (
+                        <div key={dataItem.pair}>
+                            <p>{dataItem.currency}</p>
+                            <p>{dataItem.pair}</p>
+                            <p>{dataItem.bid}</p>
+                        </div>
+                    ))
+                }
+            </section>
         </main>
     );
 };
